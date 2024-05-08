599293875d3c38fe5253c47c04a4011b6caae49b
meMo259865 <90175732+meMo259865@users.noreply.github.com>
2024-05-09 00:59:27 +0300

    bishoy
    reem

--- School-Management-System-Inspiration-master/src/main/Home_Data.java
+++ School-Management-System-Inspiration-master/src/main/Home_Data.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package main;
 
-/**
- *
- * @author proxc
- */
 public class Home_Data extends javax.swing.JFrame {
 
     /**
